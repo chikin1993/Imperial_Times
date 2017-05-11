aa0e2924fad8fc0f262aee16befb3245563514f5
Daniel Catlin <chikin1993@hotmail.co.uk>
2017-05-11 16:42:43 +0100

    fixed api keys

--- imperial_times_tweeter.py
+++ imperial_times_tweeter.py
@@ -10,6 +10,12 @@ import time
 from os import environ
 import imperial_times_news
 
+# Getting API keys from the Heroku environ
+consumer_key = os.environ['consumer_key']
+consumer_secret = os.environ['consumer_secret']
+access_token = os.environ['access_token']
+access_token_secret = os.environ['access_token_secret']
+
 # Set up OAuth and integrate with API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
